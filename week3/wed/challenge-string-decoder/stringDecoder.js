25978f1437b8a3574b91fabddb6bb1d2434b5be4
Hatice Kacer <haticemim17@gmail.com>
2022-11-18 08:39:36 -0600

    get command

--- week3/wed/challenge-string-decoder/stringDecoder.js
+++ week3/wed/challenge-string-decoder/stringDecoder.js
@@ -1,21 +1,35 @@
 
+// const decoder = code => {
+//   let splitString = code.split('')
+//   let wordToReturn = ''
+//   // console.log(splitString)
+//   for(let i =0;i < splitString.length;i++ ){
+//     if(!isNaN(+splitString[i])){
+//       i += +splitString[i]
+//     }
+//     else {
+//       wordToReturn += splitString[i]
+//     }
+//   }
+//   return wordToReturn
+// }
+
+//  console.log(decoder('2bna0p1mp2osl0e'))
 
 
-const decoder=(code)=>{
-  
-  let result=""
-  for(let i= 0; i<code.lenght; i++){
-    
-    if(!isNaN(code([i])))
-    {
-     let letter=code[i]
-     let  sliced=code.slice(i+1);
-      result+=sliced[letter]
-    
-    }
+const decoder=code=>{
+let stringSplit= code.split('');
+let wordToReturn=''
+
+for (let i= 0; i<stringSplit.length; i++){
+
+  if(!isNaN(+stringSplit[i])){
+    i += +stringSplit[i]
+  }else{
+    wordToReturn += stringSplit[i]
   }
-  return result;
+}
+return wordToReturn
 
 }
-console.log(decoder('0h2xce5ngbrdy'))
-console.log('Hello')
+console.log(decoder('2bna0p1mp2osl0e'))
